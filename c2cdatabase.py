082f1627e48b41ebf90a737eea4a0a33e38ff2a6
Kevin Chen <usa.kevin@gmail.com>
2020-07-06 15:08:07 -0500

    Changed so that the keys for both inStudentDict{} and volStudentDict{} are now email addresses instead of combined first + last names. compatibleMatches{} now holds mentee email address as keys and corresponding mentor email address as value

--- c2cdatabase.py
+++ c2cdatabase.py
@@ -40,10 +40,10 @@ incomingData = inwks.get_all_records()
 volunteerData = volwks.get_all_records()
 #############################################################################
 
-# A dictionary that will have incoming students' names as keys and their respective Student object as values
+# A dictionary that will have incoming students' emails and their respective Student object as values
 inStudentDict = {}
 
-# A dictionary that will have volunteer names as keys and their respective Student object as values
+# A dictionary that will have volunteer emails as keys and their respective Student object as values
 volStudentDict = {}
 
 # A function that makes a Student object using a list of attributes each time it is called
@@ -56,7 +56,7 @@ def createInStudentDict(data):
     for attributes in data:
         attributes = list(attributes.values())
         inStudent = makeStudent(attributes)
-        inStudentDict[inStudent.getFirstName() + inStudent.getLastName()] = inStudent
+        inStudentDict[inStudent.getEmail()] = inStudent
 
     return inStudentDict
 
@@ -65,13 +65,13 @@ def createVolStudentDict(data):
     for attributes in data:
         attributes = list(attributes.values())
         volStudent = makeStudent(attributes)
-        volStudentDict[volStudent.getFirstName() + volStudent.getLastName()] = volStudent
+        volStudentDict[volStudent.getEmail()] = volStudent
 
     return volStudentDict
 
-# Creates a new key-value pair in an incoming student's dictionary where the key is the volunteer student's name and the value is their compatability
+# Creates a new key-value pair in an incoming student's dictionary where the key is the volunteer student's email and the value is their compatability
 def makePair(volStudent, points, possiblePairs):
-    possiblePairs[volStudent.getFirstName() + volStudent.getLastName()] = points
+    possiblePairs[volStudent.getEmail()] = points
     return possiblePairs
 
 # Uses a series of if-statements and a couple methods to return a number representing the compatibility between two students.
@@ -112,35 +112,34 @@ def getCompatibility(inStudent, volStudent):
     return points
 
 #This function sends emails to all the incoming students and volunteers. matchesDict holds all the matches, and the two dictionaries are passed in
-# so that we can input key values(combined first and last name) and get the corresponding Student objects(so we can get info like their email addresses).
+# so that we can input key values(email addresses) and get the corresponding Student objects(so we can get info like their names).
 def sendEmails(matchesDict, incomingStudentDict, volunteerStudentDict):
 
     # This makes one of us type in the password so the password isn't in the script
     password = input("The password for coming2carleton@gmail.com: ")
 
     # This loops through the keys of matchesDict, which are the combined first and last names of incoming students.
-    for incomingStudentName in matchesDict:
+    for incomingStudentEmail in matchesDict:
+
+        # gets the volunteer student email associated with this incoming student
+        volStudentAddress = matchesDict[incomingStudentEmail]
 
         # get the Student objects associated with this pairing
-        incomingStudent = incomingStudentDict[incomingStudentName]
-        volStudent = volunteerStudentDict[matchesDict[incomingStudentName]]
+        incomingStudent = incomingStudentDict[incomingStudentEmail]
+        volStudent = volunteerStudentDict[volStudentAddress]
 
-        # gets both email addresses
-        inStudentAddress = incomingStudent.getEmail()
-        volStudentAddress = volStudent.getEmail()
+        menteeMsg = "Welcome to Carleton! . . . your mentor's name is " + volStudent.getFirstName() + " " +\
+        volStudent.getLastName() + " and their email is: " + volStudentAddress + " and here's more info blah blah blah"
 
-        mentorMsg = "Welcome to Carleton! . . . your mentor's name is " + volStudent.getFirstName() + " " +\
-        volStudent.getLastName() + " and their email is: " + volStudent.getEmail() + " and here's more info blah blah blah"
-
-        menteeMsg = "Thank you for signing up to be a mentor for this year's Coming2Carleton program . . . your mentee's name is " + \
-        incomingStudent.getFirstName() + " " + incomingStudent.getLastName() + " and their email is: " + incomingStudent.getEmail() + " and here's more info "
+        mentorMsg = "Thank you for signing up to be a mentor for this year's Coming2Carleton program . . . your mentee's name is " + \
+        incomingStudent.getFirstName() + " " + incomingStudent.getLastName() + " and their email is: " + incomingStudentEmail + " and here's more info "
 
     # email incoming students with a desired message
         msg = EmailMessage()
         msg['Subject'] = "Information for C2C 2021 :)"
         msg['From'] = "coming2carleton@gmail.com"
-        msg['To'] = inStudentAddress
-        msg.set_content(mentorMsg)
+        msg['To'] = incomingStudentEmail
+        msg.set_content(menteeMsg)
 
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
             smtp.login("coming2carleton@gmail.com", password)
@@ -153,7 +152,7 @@ def sendEmails(matchesDict, incomingStudentDict, volunteerStudentDict):
         msg['Subject'] = "Information for C2C 2021 :)"
         msg['From'] = "coming2carleton@gmail.com"
         msg['To'] = volStudentAddress
-        msg.set_content(menteeMsg)
+        msg.set_content(mentorMsg)
 
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
             smtp.login("coming2carleton@gmail.com", password)
@@ -164,21 +163,21 @@ def sendEmails(matchesDict, incomingStudentDict, volunteerStudentDict):
 def enterData():
     pass
 
-# Finds each incoming student's best match and puts into a dictionary called compatibleMatchesDict, where keys = incoming student's combined first + last name)
-# and values = volunteer student's combined first + last name)
+# Finds each incoming student's best match and puts into a dictionary called compatibleMatchesDict, where keys = incoming student's email address)
+# and values = volunteer student's email address)
 def findMatches(incomingStudents, volunteerStudents):
     compatibleMatchesDict = {}
 
-    # Iterating through the values of incomingStudents, a dictionary with incoming students' combined first + last names as keys
+    # Iterating through the values of incomingStudents, a dictionary with incoming students' email addresses
     # and their respective Student objects as keys.
     for inStudent in incomingStudents.values():
 
-        # Each incoming student gets a dictionary, possiblePairs, which has keys(volunteer's name) and values(volunteer's compatability with the incoming student
+        # Each incoming student gets a dictionary, possiblePairs, which has keys(volunteer's email address and values(volunteer's compatability with the incoming student
         possiblePairs = {}
 
         # This second for-loop iterates through each volunteer student for every iteration of the outer loop
         # Every incoming student is compared with every volunteer student by iterating through the values of volunteerStudents,
-        # a dictionary with keys = combined first and last names, values = their respective Student object
+        # a dictionary with keys = volunteer email address, values = their respective Student object
         for volStudent in volunteerStudents.values():
 
             compatibility = getCompatibility(inStudent, volStudent)
@@ -187,23 +186,12 @@ def findMatches(incomingStudents, volunteerStudents):
             makePair(volStudent, compatibility, possiblePairs)
 
         # Finds the volunteer with the highest compatability
-        # The variable is a string holding the combined first name + last name of that volunteer
-        compatibleVolunteerName = max(possiblePairs.items(), key = operator.itemgetter(1))[0]
+        # The variable is a string holding the email address of that volunteer
+        compatibleVolunteerEmail = max(possiblePairs.items(), key = operator.itemgetter(1))[0]
 
-        # This adds a key(incoming student's first + last name) and a value(their compatible volunteer) to compatibleMatchesDict.
+        # This adds a key(incoming student's email) and a value(their compatible volunteer's email) to compatibleMatchesDict.
         # After the outer loop is done running, this will contain all compatible matches.
-        compatibleMatchesDict[inStudent.getFirstName() + inStudent.getLastName()] = compatibleVolunteerName
-
-        # commented out print statement for testing purposes
-        '''
-        print(inStudent.getFirstName(), " is compatible with ", volunteerStudents[compatibleVolunteer].getFirstName(), ". \
-        \nTheir compatability score is: ", possiblePairs[compatibleVolunteer], "\nNow, ", \
-        volunteerStudents[compatibleVolunteer].getFirstName(), " has to email ", inStudent.getFirstName(), ". \n", inStudent.getFirstName(), \
-        "'s email is: ", inStudent.getEmail(), ". \n", volunteerStudents[compatibleVolunteer].getFirstName(), "'s email is: ", \
-        volunteerStudents[compatibleVolunteer].getEmail(), sep = "")
-
-        print('')
-        '''
+        compatibleMatchesDict[inStudent.getEmail()] = compatibleVolunteerEmail
 
     return compatibleMatchesDict
 
@@ -212,13 +200,13 @@ def findMatches(incomingStudents, volunteerStudents):
 # Then, it uses the findMatches function to compare the two lists and find each incoming student's
 # most compatible mentor and adds into a dictionary that holds the compatible matches.
 def main():
-    # Creates two dictionaries: one that will have incoming students' names as keys and their respective Student object as values
-    # and one that will have volunteer names as keys and their respective Student object as values
+    # Creates two dictionaries: one that will have incoming student email addresses and their respective Student object as values
+    # and one that will have volunteer email addresses and their respective Student object as values
     incomingStudents = createInStudentDict(incomingData)
     volunteerStudents = createVolStudentDict(volunteerData)
 
-    # finds best match for each incoming student and stores it in a dictionary with keys(incoming student's combined first + last name)
-    # and values(volunteer student's combined first + last name)
+    # finds best match for each incoming student and stores it in a dictionary with keys(incoming student email address)
+    # and values(volunteer student email address)
     matches = findMatches(incomingStudents, volunteerStudents)
 
     # sends emails to all mentors and mentees
